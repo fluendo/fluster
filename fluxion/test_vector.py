21fbce37b16e6e252af341b866f984aface22999
Pablo Marcos Oltra <pmarcos@fluendo.com>
2020-05-21 13:59:07 +0200

    Add copyright to every source file

--- fluxion/test_vector.py
+++ fluxion/test_vector.py
@@ -1,3 +1,22 @@
+# fluxion - testing framework for codecs
+# Copyright (C) 2020, Fluendo, S.A.
+#  Author: Pablo Marcos Oltra <pmarcos@fluendo.com>, Fluendo, S.A.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
 class TestVector:
     NAME = 'name'
     SOURCE = 'source'
