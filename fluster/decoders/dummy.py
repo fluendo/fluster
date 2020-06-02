a97954b55a77f96da65ff0791ae941e99ef88cd6
Pablo Marcos Oltra <pmarcos@fluendo.com>
2020-06-02 17:15:02 +0200

    Add dummy decoder

---
+++ fluster/decoders/dummy.py
@@ -0,0 +1,34 @@
+# fluster - testing framework for codecs
+# Copyright (C) 2020, Fluendo, S.A.
+#  Author: Pablo Marcos Oltra <pmarcos@fluendo.com>, Fluendo, S.A.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import hashlib
+
+from fluster.codec import Codec
+from fluster.decoder import Decoder, register_decoder
+
+
+@register_decoder
+class Dummy(Decoder):
+    '''Dummy decoder implementation'''
+    name = "Dummy"
+    codec = Codec.Dummy
+    description = "This is a dummy implementation for the dummy codec"
+
+    def decode(self, input_filepath: str, output_filepath: str, timeout: int):
+        return hashlib.md5(input_filepath.encode('utf-8')).hexdigest()
