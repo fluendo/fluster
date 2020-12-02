c2fe7dd6076e002f51b92fdd907d6d310bf50fbe
Pablo Marcos Oltra <pmarcos@fluendo.com>
2020-12-02 10:43:03 +0100

    Add libvpx-VP8 reference decoder

---
+++ fluster/decoders/libvpx.py
@@ -0,0 +1,39 @@
+# Fluster - testing framework for decoders conformance
+# Copyright (C) 2020, Fluendo, S.A.
+#  Author: Pablo Marcos Oltra <pmarcos@fluendo.com>, Fluendo, S.A.
+#  Author: Andoni Morales Alastruey <amorales@fluendo.com>, Fluendo, S.A.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+from fluster.codec import Codec, PixelFormat
+from fluster.decoder import Decoder, register_decoder
+from fluster.utils import file_checksum, run_command
+
+
+@register_decoder
+class VP8Decoder(Decoder):
+    '''VP8 reference decoder implementation'''
+    name = "libvpx-VP8"
+    description = "VP8 reference decoder"
+    codec = Codec.VP8
+    binary = 'vpxdec'
+
+    def decode(self, input_filepath: str, output_filepath: str, output_format: PixelFormat, timeout: int,
+               verbose: bool) -> str:
+        '''Decodes input_filepath in output_filepath'''
+        run_command([self.binary, '--i420', input_filepath, '-o',
+                     output_filepath], timeout=timeout, verbose=verbose)
+        return file_checksum(output_filepath)
