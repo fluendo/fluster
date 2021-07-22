277d9a9d74188b3f299c9e1969ed2fe6356e0915
Michalis Dimopoulos <mdimopoulos@fluendo.com>
2021-07-22 15:35:00 +0200

    Add AAC reference decoder.
    +decoder.py: modified decoder() function argument sequence
    
    +aac_dec.py: added a wrapper for the reference aac decoder
    
    +Makefile: changed target names to be more precise and
    replaced 2 targets which were not building executables/files
    with variables that can be executed at runtime.
    
    OCP-2317:
    Fixed fdk_aac.py::decode() signature
    to match the abstract method

---
+++ fluster/decoders/fdk_aac.py
@@ -0,0 +1,40 @@
+# Fluster - testing framework for decoders conformance
+# Copyright (C) 2021, Fluendo, S.A.
+#  Author: Michalis Dimopoulos <mdimopoulos@fluendo.com>, Fluendo, S.A.
+#  Author: Pablo Marcos Oltra <pmarcos@fluendo.com>, Fluendo, S.A.
+#  Author: Andoni Morales Alastruey <amorales@fluendo.com>, Fluendo, S.A.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+from fluster.codec import Codec, PixelFormat
+from fluster.decoder import Decoder, register_decoder
+from fluster.utils import file_checksum, run_command
+
+
+@register_decoder
+class FDKAACDecoder(Decoder):
+    '''FDK AAC reference decoder implementation'''
+    name = "FDK-AAC"
+    description = "FDK AAC reference decoder"
+    codec = Codec.AAC
+    binary = 'aac-dec'
+
+    def decode(self, input_filepath: str, output_filepath: str, output_format: PixelFormat, timeout: int,
+               verbose: bool) -> str:
+        '''Decodes input_filepath in output_filepath'''
+        run_command([self.binary, input_filepath, output_filepath],
+                    timeout=timeout, verbose=verbose)
+        return file_checksum(output_filepath)
