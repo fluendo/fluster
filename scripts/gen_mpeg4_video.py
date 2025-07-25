0f5d7568b2625a45b4bbd84cb80f0570f6b7e2a5
rsanchez <rsanchez@fluendo.com>
2025-07-25 14:17:32 +0200

    build: Add MPEG4 video generator script
    - needs to fix "I accept" to download some test vector automatically
    - add new "handle_terms" parameter to manage previous point
    - add SIMPLE_PROFILE = "Simple Profile" in codec.py

---
+++ scripts/gen_mpeg4_video.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+# Fluster - testing framework for decoders conformance
+# Copyright (C) 2020-2025, Fluendo, S.A.
+#  Author: Ruben Sanchez Sanchez <rsanchez@fluendo.com>, Fluendo, S.A.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import multiprocessing
+import os
+import sys
+from html.parser import HTMLParser
+from typing import List, Optional, Tuple
+from urllib.parse import urljoin
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from fluster import utils
+from fluster.codec import Codec, OutputFormat, Profile
+from fluster.test_suite import TestSuite
+from fluster.test_vector import TestVector
+from fluster.utils import create_enhanced_opener
+
+BASE_URL = "https://standards.iso.org/ittf/PubliclyAvailableStandards/"
+RESOURCES = {
+    "simple_profile_4_5": {
+        "type": "directory",
+        "url": BASE_URL + "c041491_ISO_IEC0_14496-4_2004_Amd_10_2005_Conformance_Testing/",
+    },
+    "profile_6": {"type": "direct", "url": BASE_URL + "c046298_ISOIEC_14496-4_2004_Amd_28_2008_bitstreams.zip"},
+}
+BITSTREAM_EXTS = [".bits", ".bit", ".m4v", ".cmp", ".tgz", ".zip"]
+
+
+class HREFParser(HTMLParser):
+    def __init__(self) -> None:
+        super().__init__()
+        self.links: List[str] = []
+
+    def error(self, message: str) -> None:
+        pass
+
+    def handle_starttag(self, tag: str, attrs: List[Tuple[str, Optional[str]]]) -> None:
+        if tag == "a":
+            for name, value in attrs:
+                if name == "href" and value:
+                    if not value.startswith("javascript") and not value.startswith("mailto"):
+                        if value.startswith("http"):
+                            full_url = value
+                        else:
+                            full_url = urljoin(BASE_URL, value)
+                        self.links.append(full_url)
+
+
+class MPEG4VIDEOGenerator:
+    def __init__(
+        self,
+        name: str,
+        suite_name: str,
+        codec: Codec,
+        description: str,
+        resources: List[str],
+        use_ffprobe: bool = False,
+    ):
+        self.name = name
+        self.suite_name = suite_name
+        self.codec = codec
+        self.description = description
+        self.resources = resources
+        self.use_ffprobe = use_ffprobe
+        self.handle_terms = True
+
+    def generate(self, download: bool, jobs: int) -> None:
+        absolute_dest_dir = os.path.dirname(os.path.abspath(__file__))
+        absolute_resources_dir = os.path.join(absolute_dest_dir, "resources", self.name)
+        output_filepath = os.path.join(self.suite_name + ".json")
+        test_suite = TestSuite(
+            output_filepath,
+            absolute_resources_dir,
+            self.suite_name,
+            self.codec,
+            self.description,
+            {},
+            handle_terms=self.handle_terms,
+        )
+
+        for resource_name in self.resources:
+            resource = RESOURCES.get(resource_name)
+            if not resource:
+                continue
+            urls = []
+            if resource["type"] == "directory":
+                urls = self.get_files_in_directory(resource["url"])
+            elif resource["type"] == "direct":
+                urls = [resource["url"]]
+            for file_url in urls:
+                filename = os.path.basename(file_url)
+                if resource["type"] == "direct" or any(filename.lower().endswith(ext) for ext in BITSTREAM_EXTS):
+                    name = os.path.splitext(filename)[0]
+                    file_input = f"{name}.bin"
+                    test_vector = TestVector(name, file_url, "__skip__", file_input, OutputFormat.UNKNOWN, "")
+                    test_suite.test_vectors[name] = test_vector
+
+        if download:
+            test_suite.download(
+                jobs=jobs,
+                out_dir=test_suite.resources_dir,
+                verify=False,
+                extract_all=True,
+                keep_file=True,
+            )
+
+        original_vectors = {
+            name: {"source": vector.source, "source_checksum": vector.source_checksum}
+            for name, vector in test_suite.test_vectors.items()
+        }
+
+        test_suite.test_vectors = {}
+
+        suite_dir = os.path.join(test_suite.resources_dir, test_suite.name)
+        if os.path.exists(suite_dir):
+            for root, _, files in os.walk(suite_dir):
+                for file in files:
+                    if any(file.lower().endswith(ext) for ext in BITSTREAM_EXTS) and not file.lower().endswith(
+                        (".tgz", ".zip")
+                    ):
+                        full_path = os.path.join(root, file)
+                        rel_path = os.path.relpath(full_path, suite_dir)
+                        name = os.path.splitext(file)[0]
+
+                        # Find original vector info
+                        orig_info = next(
+                            (info for orig_name, info in original_vectors.items() if orig_name in rel_path),
+                            {"source": "", "source_checksum": ""},
+                        )
+
+                        # Clean up path: remove duplicate folders or root archive folders
+                        path_parts = rel_path.split(os.sep)
+                        if len(path_parts) >= 2 and (
+                            path_parts[0] == path_parts[1]  # Duplicate folders
+                            or any(
+                                path_parts[0].lower() in os.path.basename(info.get("source", "")).lower()
+                                for info in original_vectors.values()
+                                if info.get("source")
+                            )
+                        ):
+                            rel_path = os.sep.join(path_parts[1:])
+
+                        test_vector = TestVector(
+                            name, orig_info["source"], orig_info["source_checksum"], rel_path, OutputFormat.UNKNOWN, ""
+                        )
+
+                        if self.use_ffprobe and not any(
+                            full_path.lower().endswith(ext) for ext in [".zip", ".tgz", ".tar.gz"]
+                        ):
+                            try:
+                                ffprobe = utils.normalize_binary_cmd("ffprobe")
+                                command = [
+                                    ffprobe,
+                                    "-v",
+                                    "error",
+                                    "-select_streams",
+                                    "v:0",
+                                    "-show_entries",
+                                    "stream=profile,pix_fmt",
+                                    "-of",
+                                    "default=nokey=1:noprint_wrappers=1",
+                                    full_path,
+                                ]
+                                result = utils.run_command_with_output(command).splitlines()
+                                if len(result) >= 2:
+                                    try:
+                                        test_vector.output_format = OutputFormat[result[1].upper()]
+                                        test_vector.profile = Profile[
+                                            result[0].translate(str.maketrans(" :", "__")).upper()
+                                        ]
+                                    except KeyError:
+                                        pass
+                            except Exception:
+                                pass
+
+                        # Update checksum if source exists
+                        if test_vector.source:
+                            orig_name = next(
+                                (n for n, info in original_vectors.items() if info["source"] == test_vector.source),
+                                None,
+                            )
+                            if orig_name:
+                                downloaded_file = os.path.join(
+                                    suite_dir, orig_name, os.path.basename(test_vector.source)
+                                )
+                                if os.path.exists(downloaded_file):
+                                    test_vector.source_checksum = utils.file_checksum(downloaded_file)
+
+                        test_suite.test_vectors[name] = test_vector
+
+        absolute_output_filepath = os.path.join(absolute_dest_dir, output_filepath)
+        test_suite.to_json_file(absolute_output_filepath)
+
+    @staticmethod
+    def get_files_in_directory(directory_url: str) -> List[str]:
+        opener = create_enhanced_opener()
+        response = opener.open(directory_url)
+        data = response.read().decode("utf-8")
+
+        hparser = HREFParser()
+        hparser.feed(data)
+
+        links: List[str] = []
+        for link in hparser.links:
+            if not link.endswith("/") and directory_url in link:
+                links.append(link)
+        return links
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--skip-download",
+        help="skip extracting tarball",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        help="number of parallel jobs to use. 2x logical cores by default",
+        type=int,
+        default=2 * multiprocessing.cpu_count(),
+    )
+    args = parser.parse_args()
+
+    generator = MPEG4VIDEOGenerator(
+        "simple_profile",
+        "MPEG4_VIDEO-SimpleProfile",
+        Codec.MPEG4_VIDEO,
+        "ISO IEC 14496-4 MPEG4 video simple profile test suite",
+        ["simple_profile_4_5", "profile_6"],
+        True,
+    )
+    generator.generate(not args.skip_download, args.jobs)
