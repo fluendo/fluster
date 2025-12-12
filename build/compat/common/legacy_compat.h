7d9c797d8f71fdec1cdfd560dc119f2892af55d9
Andoni Morales Alastruey <ylatuya@gmail.com>
2025-12-12 13:44:11 +0100

    build: add support for meson to build the reference decoders

---
+++ build/compat/common/legacy_compat.h
@@ -0,0 +1,17 @@
+/*
+ * Fluster - testing framework for decoders conformance
+ * Copyright (C) 2025, Fluendo, S.A.
+ *
+ * Compatibility header for legacy K&R C code
+ * These old codebases rely on implicit function declarations.
+ * This header provides the missing standard includes.
+ */
+
+#ifndef _COMPAT_LEGACY_H
+#define _COMPAT_LEGACY_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif /* _COMPAT_LEGACY_H */
