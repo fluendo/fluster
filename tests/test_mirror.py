0cd92ebf22ea5511729cae08e599281565e2b5f5
Alexandre Esse <alexandre.esse@canonical.com>
2026-07-10 17:46:11 +0200

    fix: update mirror script author and copyright
    Signed-off-by: Alexandre Esse <alexandre.esse@canonical.com>

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+# Fluster - testing framework for decoders conformance
+# Copyright (C) 2026, Canonical Ltd.
+#  Author: Alexandre Esse <alexandre.esse@canonical.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
 import functools
